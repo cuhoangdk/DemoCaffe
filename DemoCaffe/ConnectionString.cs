0da76170071f808421ee24b8ebe1b6d8c2823599
cuhoangdk <104707960+cuhoangdk@users.noreply.github.com>
2024-05-25 18:10:20 +0700

    push phát cuối
    push phát cuối

--- DemoCaffe/ConnectionString.cs
+++ DemoCaffe/ConnectionString.cs
@@ -10,11 +10,13 @@ namespace DemoCaffe
 	public class ConnectionString
 	{
 
-		//nhà thằng lâm
-		public static string connectionString = @"Data Source=LAPTOP-SUC4QG5V\SQLEXPRESS;Initial Catalog=CafeGocNho;Integrated Security=True";
-		//nhà thằng hoàng
-		//public static string connectionString = @"Data Source=MSI\SQLEXPRESS;Initial Catalog=CafeGocNho;Integrated Security=True;Encrypt=False";
-		//nhà thằng hiệu
+		//nhà bạn lâm
+		//public static string connectionString = @"Data Source=LAPTOP-SUC4QG5V\SQLEXPRESS;Initial Catalog=CafeGocNho;Integrated Security=True";
+		//nhà bạn hoàng
+		public static string connectionString = @"Data Source=MSI\SQLEXPRESS;Initial Catalog=CafeGocNho;Integrated Security=True;Encrypt=False";
+		//nhà bạn hiệu
 		//public static string connectionString = @"Data Source=KASSA\SQLEXPRESS;Initial Catalog=CafeGocNho;Integrated Security=True";
+		//nhà cô
+		//public static string connectionString = @"";
 	}
 }
